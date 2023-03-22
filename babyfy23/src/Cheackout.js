85a9182bb0180125e238a99297103a722aa2ae38
rollyHub <rolly_farias@hotmail.com>
2023-03-22 16:43:48 +0100

    checkout.js & css

---
+++ babyfy23/src/Cheackout.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import "./Checkout.css";
+import CheckoutProduct from './CheckoutProduct';
+import baby from "./Pictuers/checkoutpicture.jpeg";
+import Subtotal from './Subtotal';
+import { useStateValue } from "./StateProvider";
+
+
+function Checkout() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+return (
+  <div className="checkout">
+    <div className="cheackout__left">
+      <img className="checkout__ad"
+      src={baby}
+      alt="" 
+      />
+    
+  <div>
+    <h3>Hello, {user.email}</h3>
+    <h2 className="checkout__title">Your shopping Basket
+    </h2>
+
+   {/*  <CheckoutProduct
+      id='121123'
+      title='this is a test kaskd kadkadk akakdk dasdkadk adkda ka dadk ad'
+      image={"https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"}
+      price={199}
+      /> */}
+
+    {basket.map(item => (
+      <CheckoutProduct
+      id={item.id}
+      title={item.title}
+      image={item.image}
+      price={item.price}
+      />
+    ))}
+      </div>
+  </div>
+<div className="checkout__right">
+  <Subtotal />
+</div>
+</div>
+  );
+}
+
+export default Checkout
